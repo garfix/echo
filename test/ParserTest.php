239b5c53defd884749faf90d93899a8479bf64d3
Patrick van Bergen <patrick@procurios.nl>
2013-12-21 17:37:09 +0100

    Replace AP with AdjP, to distinguish it from AdvP more clearly

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -44,13 +44,13 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 		$Sentence = $Parser->parseFirstLine('I am Patrick');
 		$this->assertSame('[S [NP [pronoun i]][VP [verb am][NP [PN [propernoun Patrick]]]]]', $Sentence->getSyntaxString());
 
-		// NBar -> AP NBar
+		// NBar -> AdjP NBar
 		$Sentence = $Parser->parseFirstLine('John reads the red book');
-		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [AP [adjective red]][NBar [noun book]]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [AdjP [adjective red]][NBar [noun book]]]]]]', $Sentence->getSyntaxString());
 
-		// NBar -> AP NBar -> (AP NBar) PP
+		// NBar -> AdjP NBar -> (AdjP NBar) PP
 		$Sentence = $Parser->parseFirstLine('John reads the red book in bed');
-		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [NBar [AP [adjective red]][NBar [noun book]]][PP [preposition in][NP [NBar [noun bed]]]]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [NBar [AdjP [adjective red]][NBar [noun book]]][PP [preposition in][NP [NBar [noun bed]]]]]]]]', $Sentence->getSyntaxString());
 
 		// S => NP VP NP NP
 		$Sentence = $Parser->parseFirstLine("John gives Mary flowers.");
@@ -62,9 +62,9 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 		$Parser = new Parser();
 		$Parser->setCurrentGrammar(GrammarFactory::getGrammar('en'));
 
-		// AP -> degreeAdverb adjective
+		// AdjP -> degreeAdverb adjective
 		$Sentence = $Parser->parseFirstLine('John reads the bright red book');
-		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [AP [degreeAdverb bright][adjective red]][NBar [noun book]]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb reads][NP [DP [determiner the]][NBar [AdjP [degreeAdverb bright][adjective red]][NBar [noun book]]]]]]', $Sentence->getSyntaxString());
 	}
 
 	public function testParseAdverbialPhrase()
@@ -76,11 +76,11 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 
 		// AdvP -> adverb
 		$Sentence = $Parser->parseFirstLine('John calms the fiercely barking dog');
-		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb calms][NP [DP [determiner the]][NBar [AP [AdvP [adverb fiercely]][adjective barking]][NBar [noun dog]]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb calms][NP [DP [determiner the]][NBar [AdjP [AdvP [adverb fiercely]][adjective barking]][NBar [noun dog]]]]]]', $Sentence->getSyntaxString());
 
 		// AdvP -> degreeAdverb adverb
 		$Sentence = $Parser->parseFirstLine('John calms the very fiercely barking dog');
-		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb calms][NP [DP [determiner the]][NBar [AP [AdvP [degreeAdverb very][adverb fiercely]][adjective barking]][NBar [noun dog]]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [PN [propernoun John]]][VP [verb calms][NP [DP [determiner the]][NBar [AdjP [AdvP [degreeAdverb very][adverb fiercely]][adjective barking]][NBar [noun dog]]]]]]', $Sentence->getSyntaxString());
 	}
 
 	public function testCalculatedAnswerSemantics()
@@ -89,7 +89,7 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 		$Parser->setGrammars(array(GrammarFactory::getGrammar('en')));
 
 		$Sentence = $Parser->parseFirstLine("How old was Mary Shelley when she died?");
-		$this->assertSame('[S [S [WhADVP [whwordNP how][AP [adjective old]]][auxBe was][NP [PN [propernoun Mary][propernoun Shelley]]]][SBar [whAdverb when][S [NP [pronoun she]][VP [verb died]]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [S [WhADVP [whwordNP how][AdjP [adjective old]]][auxBe was][NP [PN [propernoun Mary][propernoun Shelley]]]][SBar [whAdverb when][S [NP [pronoun she]][VP [verb died]]]]]', $Sentence->getSyntaxString());
 		$this->assertSame('manner(S1_S.complement, S1.request) and isa(S1_S.complement, Old) and tense(S1.event, Past) and name(S1.subject, "Mary Shelley") and subject(S1.event, S1.subject) and modifier(S1.event, S1_S.complement) and request(S1.request) and mood(S1.event, Interrogative) and at_time(S1.event, S1_SBar.subEvent) and isa(S1_SBar_S.subject, Female) and reference(S1_SBar_S.subject) and isa(S1_SBar.subEvent, Die) and subject(S1_SBar.subEvent, S1_SBar_S.subject) and object(S1_SBar.subEvent, S1_SBar_S.object) and mood(S1_SBar.subEvent, Declarative)', $Sentence->getSemanticsString());
 	}
 
@@ -99,13 +99,13 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 		$Parser->setGrammars(array(GrammarFactory::getGrammar('nl')));
 
 		$Sentence = $Parser->parseFirstLine("Ik ben 43 jaar oud.");
-		$this->assertSame('[S [NP [pronoun ik]][VP [copula ben][AP [NP [DP [numeral 43]][NBar [noun jaar]]][adjective oud]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [pronoun ik]][VP [copula ben][AdjP [NP [DP [numeral 43]][NBar [noun jaar]]][adjective oud]]]]', $Sentence->getSyntaxString());
 		$this->assertSame('firstPerson(S.subject) and determiner(S.object, "43") and isa(S.object, Year) and isa(S.object, Old) and subject(S.event, S.subject) and object(S.event, S.object) and mood(S.event, Declarative)', $Sentence->getSemanticsString());
 
 		$Parser->setGrammars(array(GrammarFactory::getGrammar('en')));
 
 		$Sentence = $Parser->parseFirstLine("I am 43 years old.");
-		$this->assertSame('[S [NP [pronoun i]][VP [copula am][AP [NP [DP [numeral 43]][NBar [noun years]]][adjective old]]]]', $Sentence->getSyntaxString());
+		$this->assertSame('[S [NP [pronoun i]][VP [copula am][AdjP [NP [DP [numeral 43]][NBar [noun years]]][adjective old]]]]', $Sentence->getSyntaxString());
 		$this->assertSame('firstPerson(S.subject) and determiner(S.object, "43") and isa(S.object, Year) and isa(S.object, Old) and subject(S.event, S.subject) and object(S.event, S.object) and mood(S.event, Declarative)', $Sentence->getSemanticsString());
 	}
 
