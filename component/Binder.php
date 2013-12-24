4c784986ec7dfc92f58403e580dd08bc26bee8ab
Patrick van Bergen <patrick@procurios.nl>
2013-12-24 20:22:57 +0100

    Some cleaning up.

--- component/Binder.php
+++ component/Binder.php
@@ -4,7 +4,6 @@ namespace agentecho\component;
 
 use agentecho\datastructure\Predication;
 use agentecho\datastructure\PredicationList;
-use agentecho\datastructure\Property;
 use agentecho\datastructure\Variable;
 
 /**
@@ -12,60 +11,6 @@ use agentecho\datastructure\Variable;
  */
 class Binder
 {
-	/**
-	 * Given a $Relation, replace all the properties in its arguments with the corresponding binding in $bindings.
-	 *
-	 * For example:
-	 *
-	 *  subject(S.event, S.subject)
-	 *  ['S.event' => ?e]
-	 *
-	 * yields
-	 *
-	 *  subject(?e, S.subject)
-	 *
-	 * @param Predication $Relation
-	 * @param array $bindings
-	 * @return Predication If replacements were made, a new Predication, otherwise $Relation.
-	 */
-	public static function bindRelationProperties(Predication $Relation, array &$bindings)
-	{
-		$replacementsMade = false;
-		$NewRelation = $Relation;
-
-		foreach ($Relation->getArguments() as $index => $Argument) {
-
-			if ($Argument instanceof Property) {
-
-				$propertyName = (string)$Argument;
-
-				if (isset($bindings[$propertyName])) {
-
-					if (!$replacementsMade) {
-						$NewRelation = $Relation->createClone();
-					}
-
-					$NewRelation->setArgument($index, $bindings[$propertyName]);
-					$replacementsMade = true;
-
-				}
-			}
-		}
-
-		return $NewRelation;
-	}
-
-	public static function bindRelationsProperties(PredicationList $Relations, array &$bindings)
-	{
-		$NewList = new PredicationList();
-
-		foreach ($Relations->getPredications() as $Relation) {
-			$NewList->addPredication(self::bindRelationProperties($Relation, $bindings));
-		}
-
-		return $NewList;
-	}
-
 	/**
 	 * Given a $Relation, replace all the properties in its arguments with the corresponding binding in $bindings.
 	 *
--- datastructure/DataMapping.php
+++ datastructure/DataMapping.php
@@ -19,9 +19,6 @@ class DataMapping
 	/** @var PredicationList $PostList */
 	private $PostList;
 
-//	/** @var AssignmentList $Transformations */
-//	private $Transformations = null;
-
 	/**
 	 * @param PredicationList $PostList
 	 */
@@ -54,34 +51,8 @@ class DataMapping
 		return $this->PreList;
 	}
 
-//	/**
-//	 * @param \agentecho\datastructure\AssignmentList $Transformations
-//	 */
-//	public function setTransformations($Transformations)
-//	{
-//		$this->Transformations = $Transformations;
-//	}
-//
-//	/**
-//	 * @return \agentecho\datastructure\AssignmentList
-//	 */
-//	public function getTransformations()
-//	{
-//		return $this->Transformations;
-//	}
-
 	public function __toString()
 	{
 		return $this->PreList . ' => ' . $this->PostList;
-//			(is_null($this->Transformations) ? '' : ', ' . $this->Transformations);
-	}
-
-	public function createClone()
-	{
-		$Clone = new GoalClause();
-		$Clone->setPreList($this->PreList->createClone());
-		$Clone->setPostList($this->PostList->createClone());
-//		$Clone->setTransformations($this->Transformations->createClone());
-		return $Clone;
 	}
 }
