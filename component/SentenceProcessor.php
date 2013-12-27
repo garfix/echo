85a9ba5baa1d756b791c2f61acd9702a91978737
Patrick van Bergen <patrick@procurios.nl>
2013-12-27 19:26:23 +0100

    Minor changes.

--- component/SentenceProcessor.php
+++ component/SentenceProcessor.php
@@ -145,8 +145,6 @@ class SentenceProcessor
 	 */
 	public function parse($input, Conversation $Conversation)
 	{
-		$this->send(new Event($input));
-
 		if (trim($input) == '') {
 			return [];
 		}
@@ -418,7 +416,7 @@ class SentenceProcessor
 
 		$ConjunctionVariable = new Variable('c0');
 
-		$Answer = SentenceBuilder::buildConjunction($conjuncts, $ConjunctionVariable);
+		$Answer = self::buildConjunction($conjuncts, $ConjunctionVariable);
 
 		// sentence(?c0)
 		$this->addSentenceRelation($Answer, $ConjunctionVariable);
@@ -477,6 +475,60 @@ class SentenceProcessor
 		$Relation->setArgument(1, $Arg1);
 		$Relation->setArgument(2, $Arg2);
 		$Relations->addRelation($Relation);
+	}
+
+	/**
+	 * Creates a set of conjunction relations from given entities.
+	 *
+	 * @param array $entities Constants, atoms, or variables, or a combination thereof.
+	 * @param \agentecho\datastructure\Variable $RootVariable
+	 * @throws BuildException
+	 * @return RelationList
+	 */
+	public static function buildConjunction(array $entities, Variable $RootVariable)
+	{
+		static $idGen = 0;
+
+		if (count($entities) < 2) {
+			throw new BuildException();
+        }
+
+		$Relations = new RelationList();
+		$count = count($entities);
+
+		$RightNode = new Variable('n' . ++$idGen);
+		$NameNode = $entities[$count - 1];
+
+		$Relation = new Relation();
+		$Relation->setPredicate('name');
+		$Relation->setArguments(array($RightNode, $NameNode));
+		$Relations->addRelation($Relation);
+
+		for ($i = $count - 2; $i >= 0; $i--) {
+
+			if ($i == 0) {
+				$TopNode = $RootVariable;
+			} else {
+				$TopNode = new Variable('n' . ++$idGen);
+			}
+
+			$LeftNode = new Variable('n' . ++$idGen);
+			$NameNode = $entities[$i];
+
+			$Relation = new Relation();
+			$Relation->setPredicate('name');
+			$Relation->setArguments(array($LeftNode, $NameNode));
+			$Relations->addRelation($Relation);
+
+			$Relation = new Relation();
+			$Relation->setPredicate('link');
+			$Relation->setArguments(array(new Atom('And'), $TopNode, $LeftNode, $RightNode));
+			$Relations->addRelation($Relation);
+
+			$RightNode = $TopNode;
+		}
+
+		return $Relations;
 	}
 
 	private function getFullTrace(\Exception $E)
